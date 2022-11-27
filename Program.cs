7818c84058e457d2a8f1244349c417318ecdbb5f
naoei <altenyt@gmail.com>
2022-11-27 08:16:41 -0500

    Add github cloning

--- Program.cs
+++ Program.cs
@@ -1,3 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using diffcalc_comparer.Utils;
+using Spectre.Console;
 
-Console.WriteLine("Hello, World!");
+namespace diffcalc_comparer;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        loadEnv();
+        cloneRepos();
+    }
+
+    private static void cloneRepos()
+    {
+        if (Environment.GetEnvironmentVariable("RULESET_A") == null)
+            throw new Exception("Environment variable \"RULESET_A\" must be defined.");
+        if (Environment.GetEnvironmentVariable("RULESET_B") == null)
+            throw new Exception("Environment variable \"RULESET_B\" must be defined.");
+
+        var rulesetAUrl = Environment.GetEnvironmentVariable("RULESET_A");
+        var rulesetBUrl = Environment.GetEnvironmentVariable("RULESET_B");
+        var rulesetAPath = Path.Combine(Directory.GetCurrentDirectory(), "ruleset_a");
+        var rulesetBPath = Path.Combine(Directory.GetCurrentDirectory(), "ruleset_b");
+
+        AnsiConsole.Status()
+           .StartAsync($"Cloning \"{rulesetAUrl}\" -> {rulesetAPath}", async ctx =>
+            {
+                await Github.CloneRepo(rulesetAUrl!, rulesetAPath);
+                ctx.Status = $"Cloning \"{rulesetBUrl}\" -> {rulesetBPath}";
+                await Github.CloneRepo(rulesetBUrl!, rulesetBPath);
+            }).Wait();
+    }
+
+    private static void loadEnv()
+    {
+        var filePath = Path.Combine(Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.FullName, ".env");
+
+        if (!File.Exists(filePath))
+            throw new Exception($"Could not find a \".env\" file. Make sure to copy the \".env.example\" from the root directory to \"{filePath}\"");
+
+        foreach (var line in File.ReadAllLines(filePath))
+        {
+            var split = line.Split('=', StringSplitOptions.RemoveEmptyEntries);
+
+            if (split.Length != 2)
+                continue;
+
+            Environment.SetEnvironmentVariable(split[0], split[1]);
+        }
+    }
+}
---
+++ Utils/Github.cs
@@ -0,0 +1,85 @@
+﻿using System.Text.RegularExpressions;
+using CliWrap;
+using Spectre.Console;
+
+namespace diffcalc_comparer.Utils;
+
+public static class Github
+{
+    public static async Task CloneRepo(string url, string targetDir)
+    {
+        var githubRegex = new Regex(@"^(https:\/\/github\.com\/[^\/]+\/[^\/]+)\/?([^\/]+)?\/?([^\/]+)?(\/commits\/(.*))?$");
+
+        var match = githubRegex.Match(url);
+        if (!match.Success)
+            throw new InvalidOperationException($"Couldn't clone {url} as it's an invalid URL.");
+
+        var repo = match.Groups[1].Value;
+        var target = match.Groups[2].Value;
+        var targetInfo = match.Groups[3].Value;
+        var prCommit = match.Groups[5].Value;
+
+        var cloneResult = await Cli.Wrap("git")
+                             .WithArguments(new[] { "clone", "--filter=tree:0", repo, targetDir })
+                             .WithStandardOutputPipe(PipeTarget.ToDelegate(log))
+                             .WithStandardErrorPipe(PipeTarget.ToDelegate(log))
+                             .WithValidation(CommandResultValidation.None)
+                             .ExecuteAsync();
+
+        if (cloneResult.ExitCode == 0)
+            AnsiConsole.MarkupLineInterpolated($"[dim grey]LOG:[/] Successfully cloned {url}");
+
+        switch (target)
+        {
+            case "pull":
+                var prCheckoutResult = await Cli.Wrap("gh")
+                                          .WithArguments(new[] { "pr", "checkout", targetInfo })
+                                          .WithStandardOutputPipe(PipeTarget.ToDelegate(log))
+                                          .WithStandardErrorPipe(PipeTarget.ToDelegate(log))
+                                          .WithWorkingDirectory(targetDir)
+                                          .ExecuteAsync();
+
+                if (prCheckoutResult.ExitCode == 0)
+                    AnsiConsole.MarkupLineInterpolated($"[dim grey]LOG:[/] Successfully checked out pr {targetInfo}");
+
+                if (!string.IsNullOrEmpty(prCommit))
+                {
+                    var prCheckoutResult2 = await Cli.Wrap("git")
+                                               .WithArguments(new[] { "checkout", prCommit })
+                                               .WithStandardOutputPipe(PipeTarget.ToDelegate(log))
+                                               .WithStandardErrorPipe(PipeTarget.ToDelegate(log))
+                                               .WithWorkingDirectory(targetDir)
+                                               .ExecuteAsync();
+
+                    if (prCheckoutResult2.ExitCode == 0)
+                        AnsiConsole.MarkupLineInterpolated($"[dim grey]LOG:[/] Successfully checked out {prCommit}");
+                }
+
+                break;
+
+            case "commit":
+            case "tree":
+                var checkoutResult = await Cli.Wrap("git")
+                                        .WithArguments(new[] { "checkout", targetInfo })
+                                        .WithStandardOutputPipe(PipeTarget.ToDelegate(log))
+                                        .WithStandardErrorPipe(PipeTarget.ToDelegate(log))
+                                        .WithWorkingDirectory(targetDir)
+                                        .ExecuteAsync();
+
+                if (checkoutResult.ExitCode == 0)
+                    AnsiConsole.MarkupLineInterpolated($"[dim grey]LOG:[/] Successfully checked out {targetInfo}");
+                break;
+
+            case "":
+                break;
+
+            default:
+                throw new Exception($"Not a valid repository url: {url}");
+        }
+    }
+
+    private static void log(string s)
+    {
+        AnsiConsole.MarkupLineInterpolated($"[dim grey]LOG:[/] {s}");
+    }
+}
